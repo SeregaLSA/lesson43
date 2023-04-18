feb9550a4f50b29e81f855497358998d84280953
SeregaLSA <vip.sergeylsa@mail.ru>
2023-04-18 22:20:40 +0600

    123

--- funcs.php
+++ funcs.php
@@ -33,5 +33,68 @@ function registration(): bool
         $_SESSION['errors'] = 'Ошибка регистрации';
         return false;
     }
+}
 
+function login() : bool
+{
+    global $pdo;
+
+    $login = !empty($_POST['login']) ? trim($_POST['login']) : '';
+    $pass = !empty($_POST['pass']) ? trim($_POST['pass']) : '';
+
+    if(empty($login) || empty($pass)){
+        $_SESSION['errors'] = 'Поля логин/пароль обязательны';
+        return false;
+    }
+
+    $res = $pdo->prepare("SELECT * FROM users WHERE login = ?");
+    $res->execute([$login]);
+    if(!$user = $res->fetch()){
+        $_SESSION['errors'] = 'Логин/пароль введены не верно';
+        return false;
+    }
+
+    if(!password_verify($pass, $user['pass'])){
+        $_SESSION['errors'] = 'Логин/пароль введены не верно';
+        return false;
+    }else {
+        $_SESSION['success'] = 'Успешная авторизация';
+        $_SESSION['user']['name'] = $user['login'];
+        $_SESSION['user']['id'] = $user['id'];
+        return true;
+    }
+}
+
+function save_message(){
+
+    global $pdo;
+
+    $message = !empty($_POST['message']) ? trim($_POST['message']) : '';
+
+    if(!isset($_SESSION['user']['name'])){
+        $_SESSION['errors'] = 'Необходимо авторизоваться';
+        return false;
+    }
+
+    if(empty($message)){
+        $_SESSION['errors'] = 'Введите текст сообщения';
+        return false;
+    }
+
+    $res = $pdo -> prepare("INSERT INTO messages (name, message) VALUES (?, ?)");
+    if($res -> execute(([$_SESSION['user']['name'], $message]))){
+        $_SESSION['success'] = 'Сообщение добавлено';
+        return true;
+    }else {
+        $_SESSION['errors'] = 'Ошибка!';
+        return false;
+    }
+}
+
+function get_massages()
+{
+    global $pdo;
+
+    $res = $pdo -> query("SELECT * FROM messages");
+    return $res -> fetchAll();
 }
--- index.php
+++ index.php
@@ -10,7 +10,30 @@ if(isset($_POST['register'])){
     header("Location: index.php");
     die;
 }
+
+if(isset($_POST['auth'])){
+    login();
+    header("Location: index.php");
+    die;
+}
+
+if(isset($_POST['add'])){
+    save_message();
+    header("Location: index.php");
+    die;
+}
+
+if(isset($_GET['do']) && $_GET['do'] == 'exit'){
+    if(!empty($_SESSION['user'])){
+        unset($_SESSION['user']);
+    }
+    header("Location: index.php");
+    die;
+}
+
+$messages = get_massages();
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -111,7 +134,7 @@ if(isset($_POST['register'])){
 
         <div class="row">
             <div class="col-md-6 offset-md-3">
-                <p>Добро пожаловать, User! <a href="?do=exit">Log out</a></p>
+                <p>Добро пожаловать, <?= htmlspecialchars($_SESSION['user']['name']) ?>! <a href="?do=exit">Log out</a></p>
             </div>
         </div>
 
@@ -131,20 +154,22 @@ if(isset($_POST['register'])){
 
     <?php endif;?>
 
-    <div class="row">
-        <div class="col-md-6 offset-md-3">
-            <hr>
-            <div class="card my-3">
-                <div class="card-body">
-                    <h5 class="card-title">Автор: User</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis distinctio
-                        est illum in ipsum nemo nostrum odit optio quibusdam velit. Commodi dolores dolorum ex facere
-                        maiores porro, reprehenderit velit voluptatum.</p>
-                    <p>Дата: 01.01.2000</p>
+    <?php if(!empty($messages)): ?>
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <hr>
+                <?php foreach ($messages as $message): ?>
+                <div class="card my-3">
+                    <div class="card-body">
+                        <h5 class="card-title">Автор: <?= htmlspecialchars($message['name']) ?></h5>
+                        <p class="card-text"> <?= nl2br(htmlspecialchars($message['message'])) ?> </p>
+                        <p>Дата: <?= $message['created_at'] ?> </p>
+                    </div>
                 </div>
+                <? endforeach; ?>
             </div>
         </div>
-    </div>
+    <?php endif; ?>
 
 </div>
 
